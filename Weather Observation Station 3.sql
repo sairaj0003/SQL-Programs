500660aa4d88b970731d2084a06d5facc3afd400
SAI RAJ GUDURU <140234339+sairaj0003@users.noreply.github.com>
2023-10-10 21:49:58 +0530

    Weather Observation Station 3

---
+++ Weather Observation Station 3.sql
@@ -0,0 +1,15 @@
+/*
+Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+The STATION table is described as follows:
++--------+--------------+
+| Field  | Type         |
++--------+--------------+
+| ID     | NUMBER       |
+| CITY   | VARCHAR2(21) |
+| STATE  | VARCHAR2(2)  |
+| LAT_N  | NUMBER       |
+| LONG_W | NUMBER       |
++--------+--------------+
+*/
+
+select distinct city from station where id%2=0;
