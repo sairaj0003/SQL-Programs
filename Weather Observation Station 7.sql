176c3392224a17ad646fccc64c921efa4899860b
SAI RAJ GUDURU <140234339+sairaj0003@users.noreply.github.com>
2023-10-12 18:32:49 +0530

    Weather Observation Station 7

---
+++ Weather Observation Station 7.sql
@@ -0,0 +1,18 @@
+/*
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+Input Format
+The STATION table is described as follows:
++--------+--------------+
+| Field  | Type         |
++--------+--------------+
+| ID     | NUMBER       |
+| CITY   | VARCHAR2(21) |
+| STATE  | VARCHAR2(2)  |
+| LAT_N  | NUMBER       |
+| LONG_W | NUMBER       |
++--------+--------------+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+*/
+
+select distinct city from station where right(city, 1) in ('a', 'e', 'i', 'o', 'u');
